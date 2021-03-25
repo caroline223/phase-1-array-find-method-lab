12c9895c761af290accd5d2ef2413401263dba4b
caroline223 <carolineforrester22@gmail.com>
2021-03-24 23:39:28 -0400

    commit tree 27e472b65118bccd9d6ff4593657316fb2ddc1fb

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(record){
+    const result = record.find( record => record.result === 'W');
+    if(Boolean(result) === true) {
+        return result.year;
+    }
+    else {
+            return undefined;   
+    }  
+}
